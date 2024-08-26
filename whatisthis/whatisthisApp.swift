df822013513b6a82cfd04fb3b0cc9000d5dbb3b3
Mehmet Kamay <mehmetkamay506@gmail.com>
2024-08-27 01:45:50 +0300

    Initial Commit

---
+++ whatisthis/whatisthisApp.swift
@@ -0,0 +1,17 @@
+//
+//  whatisthisApp.swift
+//  whatisthis
+//
+//  Created by Mehmet Kamay on 27.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct whatisthisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
